bb6a6a245447deb718c15e024532dc354e8c5f62
MaKiPL <makipol@gmail.com>
2018-08-06 09:52:42 +0200

    Added replacing exact same resolution .PNG

--- FLB_tool/Form1.Designer.cs
+++ FLB_tool/Form1.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.AllowDrop = true;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(490, 253);
+            this.splitContainer1.SplitterDistance = 107;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.AllowDrop = true;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.AllowDrop = true;
+            this.splitContainer2.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.flowLayoutPanel1);
+            this.splitContainer2.Size = new System.Drawing.Size(107, 253);
+            this.splitContainer2.SplitterDistance = 97;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "OPEN FLB"});
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(107, 97);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(107, 152);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Open FLB";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(3, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export .PNG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(3, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Replace .PNG";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(379, 253);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(490, 253);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "FFVI - FLB tool by Maki (Marcin Gomulak)";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- FLB_tool/Form1.cs
+++ FLB_tool/Form1.cs
@@ -6,15 +6,148 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace FLB_tool
 {
     public partial class Form1 : Form
     {
+        private uint[] imchSpritePointers;
+        private uint imchPointer;
+        private Bitmap[] sprites;
+        private string lastOpenPath;
+
+        ushort width = 0;
+        ushort height = 0;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            //string[] test = e.Data.GetFormats(false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string pathFilename;
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Final Fantasy VI FLB (*.FLB)|*.FLB";
+                ofd.ShowDialog();
+                ofd.Multiselect = false;
+                if (string.IsNullOrWhiteSpace(ofd.FileName)) return;
+                pathFilename = ofd.FileName;
+            }
+            lastOpenPath = pathFilename;
+            FileStream fs = new FileStream(pathFilename, FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs);
+            if (Encoding.UTF8.GetString(br.ReadBytes(4), 0, 4) != "FLBD") { MessageBox.Show("Wrong or corrupted FLB file"); return; }
+            fs.Seek(4, SeekOrigin.Current); //pass 0101
+            imchPointer = br.ReadUInt32();
+            fs.Seek(imchPointer, SeekOrigin.Begin);
+            if (Encoding.UTF8.GetString(br.ReadBytes(4), 0, 4) != "IMCH") { MessageBox.Show("Wrong or corrupted FLB file"); return; }
+            uint imchSize = br.ReadUInt32();
+            uint imchSpriteSize = br.ReadUInt32();
+            uint[] imchSpritePointers = new uint[imchSpriteSize];
+            sprites = new Bitmap[imchSpriteSize];
+            for (int i = 0; i < imchSpriteSize; i++)
+            {
+                uint pointer = br.ReadUInt32();
+                imchSpritePointers[i] = imchPointer + imchSize + pointer;
+            }
+            //==read sprites to bitmap==
+            for (int i = 0; i < imchSpritePointers.Length; i++)
+            {
+                fs.Seek(imchSpritePointers[i] + 24, SeekOrigin.Begin);
+                width = br.ReadUInt16();
+                height = br.ReadUInt16();
+
+                sprites[i] = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                BitmapData bmpData = sprites[i].LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                byte[] bmpBuffer = new byte[width * height * 4];
+                for (int n = 0; n < width * height * 4; n += 4)
+                {
+                    uint pixel = br.ReadUInt32();
+                    bmpBuffer[n] = (byte)(pixel >> 24 & 0xff);
+                    bmpBuffer[n + 1] = (byte)(pixel >> 16 & 0xff);
+                    bmpBuffer[n + 2] = (byte)(pixel >> 8 & 0xff);
+                    bmpBuffer[n + 3] = (byte)(pixel & 0xff);
+                }
+                Marshal.Copy(bmpBuffer, 0, bmpData.Scan0, bmpBuffer.Length);
+                sprites[i].UnlockBits(bmpData);
+            }
+            if (sprites.Length > 0)
+                pictureBox1.Image = sprites[0];
+
+            listBox1.DataSource = imchSpritePointers;
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            fs.Close();
+            fs.Dispose();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try { pictureBox1.Image = sprites[(sender as ListBox).SelectedIndex]; } catch { };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "PNG *.PNG|*.PNG", CheckPathExists = true })
+            {
+                sfd.ShowDialog();
+                if(sfd.FileName != null)
+                    pictureBox1.Image.Save(sfd.FileName, ImageFormat.Png);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string pngFilePath;
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "PNG file (*.PNG)|*.PNG", Multiselect=false })
+            {
+                ofd.ShowDialog();
+                if (string.IsNullOrWhiteSpace(ofd.FileName)) return;
+                pngFilePath = ofd.FileName;
+            }
+            Image pngData = Image.FromFile(pngFilePath);
+
+            if(pngData.RawFormat != ImageFormat.Png || pngData.PixelFormat != PixelFormat.Format32bppArgb)
+            {
+                MessageBox.Show("File you are trying to import is not either .PNG or is not 32 bit ARGB pixel format!");
+                return;
+            }
+            Bitmap bmp = new Bitmap(pngData);
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            byte[] buffer = new byte[bmpData.Height * bmpData.Width * bmpData.Stride];
+            Marshal.Copy(bmpData.Scan0, buffer, 0, buffer.Length);
+
+            if (pngData.Width != width || pngData.Height != height)
+                ReplaceDynamic(buffer);
+            else
+            {
+                using (FileStream fs = new FileStream(lastOpenPath, FileMode.Open, FileAccess.ReadWrite))
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    fs.Seek((int)listBox1.SelectedValue+28, SeekOrigin.Begin);
+                    bw.Write(buffer);
+                }
+            }
+
+        }
+
+        private void ReplaceDynamic(byte[] buffer)
+        {
+            throw new NotImplementedException();
         }
     }
 }
