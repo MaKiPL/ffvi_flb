c972011d8903eb1fa71b6eee5c4a193203ae21f2
Enfyve <8909014+Enfyve@users.noreply.github.com>
2021-02-27 18:35:59 -0500

    Add support for editing UI positions

---
+++ FLB_tool/FLBFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLB_tool
+{
+
+    public class FLBUIPosition
+    {
+        public int applyDeformation; // this is a boolean, but takes up 4 bytes
+
+        // These do not apply, unless applyDeformation == 1
+        public float xScale;
+        public float yScale;
+        public float xSkew;
+        public float ySkew;
+
+        public int xPos; // this value is in sub-pixels, divide by 20.0 to get pixels
+        public int yPos; // same as above
+    }
+}
--- FLB_tool/Form1.cs
+++ FLB_tool/Form1.cs
@@ -27,7 +27,9 @@ namespace FLB_tool
         private uint imchPointer;
         private Bitmap[] sprites;
         private string lastOpenPath;
+        private List<FLBUIPosition> uiPositions;
 
+        uint[] indices;
         ushort width = 0;
         ushort height = 0;
 
@@ -41,9 +43,16 @@ namespace FLB_tool
             InitializeComponent();
         }
 
-        private void Form1_DragDrop(object sender, DragEventArgs e)
+        private void File_DragDrop(object sender, DragEventArgs e)
         {
-            //string[] test = e.Data.GetFormats(false);
+            lastOpenPath = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            LoadFLBFile(lastOpenPath);
+        }
+
+        private void File_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -58,23 +67,30 @@ namespace FLB_tool
                 pathFilename = ofd.FileName;
             }
             lastOpenPath = pathFilename;
-            FileStream fs = new FileStream(pathFilename, FileMode.Open, FileAccess.ReadWrite);
+
+            LoadFLBFile(pathFilename);
+        }
+
+        private void LoadFLBFile(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
             BinaryReader br = new BinaryReader(fs);
             if (Encoding.UTF8.GetString(br.ReadBytes(4), 0, 4) != "FLBD") { MessageBox.Show("Wrong or corrupted FLB file"); return; }
             fs.Seek(4, SeekOrigin.Current); //pass 0101
-            imchPointer = br.ReadUInt32();
-
-            fs.Seek(12, SeekOrigin.Current);
-            SHCH = br.ReadUInt32();
-            SPCH = br.ReadUInt32();
-            BTCH = br.ReadUInt32();
-            ANCH0 = br.ReadUInt32();
+            uint headerSize = br.ReadUInt32();
+            fs.Seek(8, SeekOrigin.Current); //pass section count
+            imchPointer = br.ReadUInt32() + headerSize;
+            SHCH = br.ReadUInt32() + headerSize;
+            SPCH = br.ReadUInt32() + headerSize;
+            BTCH = br.ReadUInt32() + headerSize;
+            ANCH0 = br.ReadUInt32() + headerSize;
 
             fs.Seek(imchPointer, SeekOrigin.Begin);
             if (Encoding.UTF8.GetString(br.ReadBytes(4), 0, 4) != "IMCH") { MessageBox.Show("Wrong or corrupted FLB file"); return; }
             uint imchSize = br.ReadUInt32();
             uint imchSpriteSize = br.ReadUInt32();
             imchSpritePointers = new uint[imchSpriteSize];
+            indices = new uint[imchSpriteSize];
             sprites = new Bitmap[imchSpriteSize];
             for (int i = 0; i < imchSpriteSize; i++)
             {
@@ -84,7 +100,8 @@ namespace FLB_tool
             //==read sprites to bitmap==
             for (int i = 0; i < imchSpritePointers.Length; i++)
             {
-                fs.Seek(imchSpritePointers[i] + 24, SeekOrigin.Begin);
+                fs.Seek(imchSpritePointers[i] + 20, SeekOrigin.Begin);
+                indices[i] = br.ReadUInt32();
                 width = br.ReadUInt16();
                 height = br.ReadUInt16();
 
@@ -103,30 +120,79 @@ namespace FLB_tool
                 sprites[i].UnlockBits(bmpData);
             }
             if (sprites.Length > 0)
-                pictureBox1.Image = sprites[0];
+                pictureBox2.Image = sprites[0];
 
-            listBox1.DataSource = imchSpritePointers;
+            pictureBox2.Visible = true;
+
+            listBox1.DataSource = indices;
 
             button1.Enabled = true;
             button2.Enabled = true;
+
+            // Skip to ANCH0
+            fs.Seek(ANCH0 + 16, SeekOrigin.Begin);
+            uint uiPositionsPtr = br.ReadUInt32() + ANCH0 + 30 + 18;
+            fs.Seek(4, SeekOrigin.Current);
+            uint uiStringsPtr = br.ReadUInt32() + uiPositionsPtr - 4;
+            fs.Seek(uiPositionsPtr, SeekOrigin.Begin);
+
+            int count = br.ReadInt32(); // get number of position entries;
+            uiPositions = new List<FLBUIPosition>(count);
+            int[] uiItems = new int[count];
+
+            for(int i = 0; i < count; i++)
+            {
+                uiPositions.Add(new FLBUIPosition() {
+                    applyDeformation = br.ReadInt32(),
+                    xScale = br.ReadSingle(),
+                    yScale = br.ReadSingle(),
+                    xSkew = br.ReadSingle(),
+                    ySkew = br.ReadSingle(),
+                    xPos = br.ReadInt32(),
+                    yPos = br.ReadInt32()
+                });
+
+                uiItems[i] = i;
+            }
+
+            uiPositionsLB.DataSource = uiItems;
+
+
+            // Read UI strings
+            fs.Seek(uiStringsPtr, SeekOrigin.Begin);
+            count = br.ReadInt32();
+            var uiStrings = new List<string>(count);
+
+            for(int i = 0; i < count; i++)
+            {
+                string s = Encoding.ASCII.GetString(br.ReadBytes(0x80));
+                fs.Seek(4, SeekOrigin.Current);
+
+                uiStrings.Add(s);
+            }
+
+            uiStringsLB.DataSource = uiStrings;
+
             fs.Close();
             fs.Dispose();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            try
-            {
-                pictureBox1.Image = sprites[(sender as ListBox).SelectedIndex];
-                width = (ushort)pictureBox1.Image.Width;
-                height = (ushort)pictureBox1.Image.Height;
+            try {
+                pictureBox2.Image = sprites[(sender as ListBox).SelectedIndex];
+                width = (ushort)pictureBox2.Image.Width;
+                height = (ushort)pictureBox2.Image.Height;
+                pictureBox2.Width = width;
+                pictureBox2.Height = height;
+                
             }
             catch { };
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (pictureBox1.Image == null) return;
+            if (pictureBox2.Image == null) return;
             using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "PNG *.PNG|*.PNG", CheckPathExists = true })
             {
                 sfd.ShowDialog();
@@ -135,9 +201,9 @@ namespace FLB_tool
                     if (File.Exists(sfd.FileName))
                     {
                         File.Delete(sfd.FileName);
-                        pictureBox1.Image.Save(sfd.FileName, ImageFormat.Png);
+                        pictureBox2.Image.Save(sfd.FileName, ImageFormat.Png);
                     }
-                    else pictureBox1.Image.Save(sfd.FileName, ImageFormat.Png);
+                    else pictureBox2.Image.Save(sfd.FileName, ImageFormat.Png);
                 }
             }
         }
@@ -260,6 +326,195 @@ namespace FLB_tool
                 Buffer.BlockCopy(fullBuffer, (int)SHCH, finalBuffer, dstOffset, cnt-12);
             }
             File.WriteAllBytes(lastOpenPath, finalBuffer);
+        }
+
+        private void uiPositionsLB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = (sender as ListBox).SelectedIndex;
+            xOffset.Value = (decimal)(uiPositions[index].xPos / 20.0);
+            yOffset.Value = (decimal)(uiPositions[index].yPos / 20.0);
+            xScale.Value = (decimal)uiPositions[index].xScale;
+            yScale.Value = (decimal)uiPositions[index].yScale;
+            xSkew.Value = (decimal)uiPositions[index].xSkew;
+            ySkew.Value = (decimal)uiPositions[index].ySkew;
+
+            applyDeform.Checked = (uiPositions[index].applyDeformation == 1);
+
+
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            int index = uiPositionsLB.SelectedIndex;
+            if (enableTransforming.Checked) 
+            {
+                pictureBox2.Location = new Point(uiPositions[index].xPos / 20, uiPositions[index].yPos / 20);
+            } 
+            else
+            {
+                pictureBox2.Location = new Point(0, 0);
+            }
+
+            if (applyDeform.Checked && enableTransforming.Checked)
+            {
+                pictureBox2.Width = (int)Math.Round(width * uiPositions[index].xScale);
+                pictureBox2.Height = (int)Math.Round(height * uiPositions[index].yScale);
+            }
+            else
+            {
+                pictureBox2.Width = width;
+                pictureBox2.Height = height;
+            }
+        }
+
+        private void applyDeform_CheckedChanged(object sender, EventArgs e)
+        {
+            var isChecked = (sender as CheckBox).Checked;
+            deformationGroup.Enabled = isChecked;
+
+            int i = uiPositionsLB.SelectedIndex;
+            uiPositions[i].applyDeformation = isChecked ? 1 : 0;
+        }
+
+        private void saveANCH0_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(lastOpenPath, FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs);
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            fs.Seek(ANCH0 + 16, SeekOrigin.Begin);
+            uint uiPosPtr = br.ReadUInt32() + ANCH0 + 30 + 18;
+
+            fs.Seek(uiPosPtr, SeekOrigin.Begin);
+
+            int count = br.ReadInt32();
+
+            for (int i = 0; i < count; i++)
+            {
+                bw.Write(uiPositions[i].applyDeformation);
+                bw.Write(uiPositions[i].xScale);
+                bw.Write(uiPositions[i].yScale);
+                bw.Write(uiPositions[i].xSkew);
+                bw.Write(uiPositions[i].ySkew);
+                bw.Write(uiPositions[i].xPos);
+                bw.Write(uiPositions[i].yPos);
+            }
+            bw.Flush();
+            fs.Close();
+            fs.Dispose();
+        }
+
+        private void reloadANCH0_Click(object sender, EventArgs e)
+        {
+            int prevUIPosIndex = uiPositionsLB.SelectedIndex;
+            int prevUIStringsIndex = uiStringsLB.SelectedIndex;
+
+            FileStream fs = new FileStream(lastOpenPath, FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs);
+
+
+            // Skip to ANCH0
+            fs.Seek(ANCH0 + 16, SeekOrigin.Begin);
+            uint uiPositionsPtr = br.ReadUInt32() + ANCH0 + 30 + 18;
+            fs.Seek(4, SeekOrigin.Current);
+            uint uiStringsPtr = br.ReadUInt32() + uiPositionsPtr - 4;
+            fs.Seek(uiPositionsPtr, SeekOrigin.Begin);
+
+            int count = br.ReadInt32(); // get number of position entries;
+            uiPositions = new List<FLBUIPosition>(count);
+            int[] uiItems = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                uiPositions.Add(new FLBUIPosition()
+                {
+                    applyDeformation = br.ReadInt32(),
+                    xScale = br.ReadSingle(),
+                    yScale = br.ReadSingle(),
+                    xSkew = br.ReadSingle(),
+                    ySkew = br.ReadSingle(),
+                    xPos = br.ReadInt32(),
+                    yPos = br.ReadInt32()
+                });
+
+                uiItems[i] = i;
+            }
+
+            uiPositionsLB.DataSource = uiItems;
+
+
+            // Read UI strings
+            fs.Seek(uiStringsPtr, SeekOrigin.Begin);
+            count = br.ReadInt32();
+            var uiStrings = new List<string>(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                string s = Encoding.ASCII.GetString(br.ReadBytes(0x80));
+                fs.Seek(4, SeekOrigin.Current);
+
+                uiStrings.Add(s);
+            }
+
+            uiStringsLB.DataSource = uiStrings;
+
+            fs.Close();
+            fs.Dispose();
+
+            // Reapply selection index
+            uiPositionsLB.SelectedIndex = prevUIPosIndex;
+            uiStringsLB.SelectedIndex = prevUIStringsIndex;
+
+        }
+
+        private void enableTransforming_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateImage();
+        }
+
+        private void xOffset_ValueChanged(object sender, EventArgs e)
+        {
+            int i = uiPositionsLB.SelectedIndex;
+            uiPositions[i].xPos = Convert.ToInt32(xOffset.Value * 20.0M);
+
+            UpdateImage();
+        }
+
+        private void yOffset_ValueChanged(object sender, EventArgs e)
+        {
+            int i = uiPositionsLB.SelectedIndex;
+            uiPositions[i].yPos = Convert.ToInt32(yOffset.Value * 20.0M);
+
+            UpdateImage();
+        }
+
+        private void xScale_ValueChanged(object sender, EventArgs e)
+        {
+            int i = uiPositionsLB.SelectedIndex;
+            uiPositions[i].xScale = Convert.ToSingle(xScale.Value);
+
+            UpdateImage();
+        }
+
+        private void yScale_ValueChanged(object sender, EventArgs e)
+        {
+            int i = uiPositionsLB.SelectedIndex;
+            uiPositions[i].yScale = Convert.ToSingle(yScale.Value);
+
+            UpdateImage();
+        }
+
+        private void xSkew_ValueChanged(object sender, EventArgs e)
+        {
+            int i = uiPositionsLB.SelectedIndex;
+            uiPositions[i].xSkew = Convert.ToSingle(xSkew.Value);
+        }
+
+        private void ySkew_ValueChanged(object sender, EventArgs e)
+        {
+            int i = uiPositionsLB.SelectedIndex;
+            uiPositions[i].ySkew = Convert.ToSingle(ySkew.Value);
         }
     }
 }
